7b57d9323820ee565737f2109648d25f47d2f097
tshibangu-jeremie <66729840+tshibangu-jeremie@users.noreply.github.com>
2020-07-05 22:36:42 +0200

    exercice script.js suite

--- Exercices/exercice-quote-day/script.js
+++ Exercices/exercice-quote-day/script.js
@@ -1,1 +1,12 @@
 /* C'est ici qu'il faut rédiger ton code pour utiliser l'API */
+let randomNumber = Math.floor(Math.random() * Math.floor(100));
+fetch("https://type.fit/api/quotes")
+  .then(response => {
+    console.log(response)
+    return response.json();
+  })
+  .then(response => {
+    let data = response[randomNumber]
+    document.getElementById("text").textContent = data.text;
+    console.log(data.text);
+  });
